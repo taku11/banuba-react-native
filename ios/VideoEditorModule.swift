aec938d81e2335d05a74354cc2a04060d515cc32
taku11 <no9.noid6@gmail.com>
2022-03-29 17:06:26 +0900

    first commit

---
+++ ios/VideoEditorModule.swift
@@ -0,0 +1,133 @@
+//
+//  VideoEditorModule.swift
+//  vesdkreactnativeintegrationsample
+//
+//  Created by Andrei Sak on 28.12.20.
+//
+
+import React
+import BanubaVideoEditorSDK
+import BanubaMusicEditorSDK
+import BanubaOverlayEditorSDK
+import VideoEditor
+
+@objc(VideoEditorModule)
+class VideoEditorModule: NSObject, RCTBridgeModule {
+  
+  private var videoEditorSDK: BanubaVideoEditor?
+  
+  static func requiresMainQueueSetup() -> Bool {
+    return true
+  }
+  
+  private var currentResolve: RCTPromiseResolveBlock?
+  private var currentReject: RCTPromiseRejectBlock?
+  
+  // Export callback
+  @objc func openVideoEditor(_ resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+    self.currentResolve = resolve
+    self.currentReject = reject
+    
+    let config = createVideoEditorConfiguration()
+    videoEditorSDK = BanubaVideoEditor(
+      token: "Place your video editor token here",
+      configuration: config,
+      externalViewControllerFactory: nil
+    )
+    
+    // Set delegate
+    videoEditorSDK?.delegate = self
+    
+    DispatchQueue.main.async {
+      guard let presentedVC = RCTPresentedViewController() else {
+        return
+      }
+      let config = VideoEditorLaunchConfig(
+        entryPoint: .camera,
+        hostController: presentedVC,
+        animated: true
+      )
+      self.videoEditorSDK?.presentVideoEditor(
+        withLaunchConfiguration: config,
+        completion: nil
+      )
+    }
+  }
+  
+  private func createVideoEditorConfiguration() -> VideoEditorConfig {
+    let config = VideoEditorConfig()
+    // Do customization here
+    return config
+  }
+  
+  // MARK: - RCTBridgeModule
+  static func moduleName() -> String! {
+    return "VideoEditorModule"
+  }
+}
+
+// MARK: - Export flow
+extension VideoEditorModule {
+  func exportVideo() {
+    let manager = FileManager.default
+    // File name
+    let firstFileURL = manager.temporaryDirectory.appendingPathComponent("tmp1.mov")
+    if manager.fileExists(atPath: firstFileURL.path) {
+      try? manager.removeItem(at: firstFileURL)
+    }
+    
+    // Video configuration
+    let exportVideoConfigurations: [ExportVideoConfiguration] = [
+      ExportVideoConfiguration(
+        fileURL: firstFileURL,
+        quality: .auto,
+        useHEVCCodecIfPossible: true,
+        watermarkConfiguration: nil
+      )
+    ]
+    
+    // Export Configuration
+    let exportConfiguration = ExportConfiguration(
+      videoConfigurations: exportVideoConfigurations,
+      isCoverEnabled: true,
+      gifSettings: nil
+    )
+    
+    // Export func
+    videoEditorSDK?.export(
+      using: exportConfiguration
+    ) { [weak self] (success, error, coverImage) in
+      // Export Callback
+      DispatchQueue.main.async {
+        if success {
+          // Result urls. You could interact with your own implementation.
+          self?.currentResolve!(["videoUri": firstFileURL.absoluteString])
+          // remove strong reference to video editor sdk instance
+          self?.videoEditorSDK = nil
+        } else {
+          self?.currentReject!("", error?.errorMessage, nil)
+          // remove strong reference to video editor sdk instance
+          self?.videoEditorSDK = nil
+          print("Error: \(String(describing: error))")
+        }
+      }
+    }
+  }
+}
+
+// MARK: - BanubaVideoEditorSDKDelegate
+extension VideoEditorModule: BanubaVideoEditorDelegate {
+  func videoEditorDidCancel(_ videoEditor: BanubaVideoEditor) {
+    videoEditor.dismissVideoEditor(animated: true) { [weak self] in
+      // remove strong reference to video editor sdk instance
+      self?.videoEditorSDK = nil
+      self?.currentResolve!(NSNull())
+    }
+  }
+  
+  func videoEditorDone(_ videoEditor: BanubaVideoEditor) {
+    videoEditor.dismissVideoEditor(animated: true) { [weak self] in
+      self?.exportVideo()
+    }
+  }
+}
